40e4cdad6c8933089a86d7b9151c2510d3d521b6
Luke Godfrey <91397512+LTGDev@users.noreply.github.com>
2022-11-16 20:50:02 +1100

    Initial upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordBTN1 = document.getElementById("password-btn1")
+let passwordBTN2 = document.getElementById("password-btn2")
+
+function generatePassword() {
+let password1 = [];
+let rand1 = characters[Math.floor(Math.random()*characters.length)]
+console.log(rand1)
+   for (let i = 0; i <15; i++) {
+       password1.push(characters[Math.floor(Math.random()*characters.length)])
+   }
+   passwordBTN1.textContent = password1
+
+
+let password2 = [];
+let rand2 = characters[Math.floor(Math.random()*characters.length)]
+console.log(rand2)
+   for (let i = 0; i <15; i++) {
+       password2.push(characters[Math.floor(Math.random()*characters.length)])
+   }
+passwordBTN2.textContent = password2
+}
+

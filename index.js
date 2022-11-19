5137eeb0b4fe015dd0424ba479c1af16931d7020
Luke Godfrey <91397512+LTGDev@users.noreply.github.com>
2022-11-19 16:03:53 +1100

    Update files

--- index.js
+++ index.js
@@ -5,34 +5,19 @@ let passwordBTN1 = document.getElementById("password-btn1")
 let passwordBTN2 = document.getElementById("password-btn2")
 
 function generatePassword() {
-<<<<<<< Updated upstream
-let password1 = [];
-let rand1 = characters[Math.floor(Math.random()*characters.length)]
-=======
 let password1 = "";
 let password2 = "";
->>>>>>> Stashed changes
    for (let i = 0; i <15; i++) {
        const index = Math.floor(Math.random()*characters.length)
        password1 += characters[index]
        console.log(password1)
        passwordBTN1.textContent = password1
    }
-<<<<<<< Updated upstream
-   passwordBTN1.textContent = password1
-
-
-let password2 = [];
-let rand2 = characters[Math.floor(Math.random()*characters.length)]
-   for (let i = 0; i <15; i++) {
-       password2.push(characters[Math.floor(Math.random()*characters.length)])
-=======
        
         for (let i = 0; i <15; i++) {
        const index2 = Math.floor(Math.random()*characters.length)
        password2 += characters[index2]
        console.log(password2)
        passwordBTN2.textContent = password2
->>>>>>> Stashed changes
    }
 }
